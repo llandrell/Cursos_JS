0f7dc9090ac36df40c199093a7440a2a4e36fd80
llandrell85 <44513510+llandrell@users.noreply.github.com>
2023-02-19 02:37:49 -0300

    lsita de tarefas
    json, finalizando lista de tarefas e finalizando o modulo completo

--- ListaDeTarefas/assets/js/index.js
+++ ListaDeTarefas/assets/js/index.js
@@ -21,10 +21,87 @@ function criaTarefa(textInput) {
     //ou seja adicionando meu text que contem o elemento (li) e o (html text) dentro da tarefa que está dentro da tag 
     //html .tarefa = (UL no html)
     tarefas.appendChild(li);
-    
+
+    //função para limpar o input
+    limpaInput();
+
+    //função para apagar o conteudo
+    btApagar(li);
+
+    //chamadno a função para salvar a tarefa
+    salvarTarefas();
 
 };
+//se o botão foi clicado apague o elemento 
+function limpaInput() {
+    //limpando o input atraves oassando o valor pelo .value.
+    //limpado o valor dentro do input
+    inputTarefa.value = '';
+    //um evento html, qundo clic no botão input fica psicando o cursor 
+    //nesse caso mandando o js fazer o focus, ele volta para o focus
+    inputTarefa.focus();
+    
+}
 
+
+        //se o botão foi clicado apague o elemento 
+// criando botão para apagar o conteudo da pagina 
+function btApagar(li) {
+    li.innerText += ' ';
+    const btApagar = document.createElement('button');
+    btApagar.innerText = 'Apagar';
+        //setando atributo 
+        //btApagar.classList.add('apagar'); //adiconannnnnndo um classe denro do botão
+    
+    //
+    btApagar.setAttribute('class', 'apagar');
+    li.appendChild(btApagar);   
+
+    
+    
+};
+
+//Criando a função para salvar as tarefas criadas em criaTarefas()
+function salvarTarefas() {
+    //pegando os li dentro de tarefas
+    const liTarefas = tarefas.querySelectorAll('li');
+
+    //criadno um array vazio para guardar o centeudo dos li
+    const listaDeTarefas = [];
+    
+    //criando o for para iterar sobre as nodes list (liTarefas) e guardar no array
+    //
+    for (let iTarefa of liTarefas) {
+        //pegando do texto do li que foi colocado em iTarefa e adicionando dentro de tarefaText
+        let tarefaText = iTarefa.innerText;
+        //fazendo um replace nos texto de tarefaText 
+        // subistuindo (replace) a palavra pagar por nada
+        //.trim remove o espaco sobrando na ponta da string 
+        tarefaText = tarefaText.replace('Apagar', '').trim();
+
+        // jogando o texto de tarefaText dentro da lista de tarefas
+        listaDeTarefas.push(tarefaText);
+            };
+
+    //armazenando uma do array convertido para JSON
+    //pode ser convertido de volta em array
+    const tarefasJSON = JSON.stringify(listaDeTarefas);
+    
+    //local do navegador destinado a salvar dados(um mini banco de dados)
+    //inportate no localStora so pode ser armazenado strings por isso foi convertido para json string antes.
+    localStorage.setItem('tarefas', tarefasJSON);
+};
+
+//função para carregar a 'base de dados no site' quando o site for carregado
+function addTarefasSalvas() {
+    const tarefasSalvas = localStorage.getItem('tarefas');
+    //convertendo a lista de strint para json
+    const listaDeTarefas = JSON.parse(tarefasSalvas);
+
+    for (let tarefa of listaDeTarefas) {
+        criaTarefa(tarefa);
+    }
+};
 
 // pegando o evento dentro do inputTarefa quando uma tecla for precionada.
 inputTarefa.addEventListener('keypress', function (e) {
@@ -39,6 +116,7 @@ inputTarefa.addEventListener('keypress', function (e) {
     }
     
 });
+//se o botão foi clicado apague o elemento 
 
 // capiturando o evento click ao cliclar no botão btn-add-tarefa
 btnTarefa.addEventListener('click', function (e) {
@@ -56,3 +134,23 @@ btnTarefa.addEventListener('click', function (e) {
     //pegando o valor do imput criado no html
     //console.log(imputTarefa.value);
 });
+
+
+//pegando o evento click para adicionar a função apagar
+document.addEventListener('click', function (e) {
+    const el = e.target;
+    //if para saber se o elemento é o botão de apagar
+    //se esse botão contem a class list 'apagar' é o que eu quero.
+    if (el.classList.contains('apagar')) {
+        //se o botão foi clicado apague o li
+        // quando clicar no el vou saber quem e o pai atraveis do parenteElement
+        //do meu elemento o pai dele vai ser removido. atraveis do .remove
+        el.parentElement.remove();
+        salvarTarefas()
+    
+    }
+})
+
+
+
+addTarefasSalvas()
